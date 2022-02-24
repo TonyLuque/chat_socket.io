8da5ed0e3253c407f09a63e22c3d5cb21013dc59
anthony luque <kasuru@hotmail.com>
2022-02-23 19:00:56 -0500

    [ADD] chat

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("Se coencto un usuario");
+  // socket.on("chat message", (msg) => {
+  //   console.log("message: " + msg);
+  // });
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("server listen in port 3000");
+});
