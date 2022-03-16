eb2592f345d778836cc59e2898c9bfcc4999e206
anthony luque <kasuru@hotmail.com>
2022-03-15 20:46:28 -0500

    [ADD] dotenv, nodemon and settiemout to proof notification

--- index.js
+++ index.js
@@ -4,24 +4,33 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+require("dotenv").config();
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {
-  console.log("Se coencto un usuario");
-  // socket.on("chat message", (msg) => {
-  //   console.log("message: " + msg);
-  // });
+io.on("connection", async (socket) => {
+  console.log("Se conecto un usuario");
+
   socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
     io.emit("chat message", msg);
   });
+
+  let count = 0;
+  while (count < 10) {
+    io.emit("notification", count);
+    console.log(count);
+    count++;
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 });
 
 server.listen(process.env.PORT, () => {
-  console.log("server listen in port 3000");
+  console.log(`server listen in port ${process.env.PORT}`);
 });
